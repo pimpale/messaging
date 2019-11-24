a9badbdeba19f592540774d71d320beb52d23088
gpi <gpimpale29@gmail.com>
2019-11-24 13:52:13 -0800

    runs on port 80 now + press enter to submit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 8080;
+const port = 80;
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
